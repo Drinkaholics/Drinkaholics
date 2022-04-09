98025dd8a9aa24e709b2a1139eaabd676481ca80
Alexander <alexr722@gmail.com>
2022-04-09 12:29:51 -0400

    upgrade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,104 @@
-var searchbar = document.querySelector("#ingredients");
-var searchbtn = document.querySelector("#submit-button")
-var resetbtn = document.querySelector("#reset");
-var ingredientlist = document.querySelector("#search-history");
-var currentlist = [];
+var userFormEl = document.querySelector("#search-form");
+var nameInputEl = document.querySelector("#recipeSearch");
 
-var displayonsearch = function() {
-  if (currentlist.length == 0) {
-    console.log("No Search and Default Display");
-    return;
-  } else {
-    var ingredient =  "";
-    for (var i = 0; i < currentlist.length; i++) {
-      ingredient += currentlist[i] + ",";
+//Global Variables
+var apiKey = "76fc45feadbe46379e4c23a107066a2f";
+
+var formSumbitHandler = function(event) {
+    //Prevents browser from sending the form's input data to a URL
+    event.preventDefault();
+
+    //get value from input element
+    var recipeSearch = nameInputEl.value.trim();
+
+    if (recipeSearch) {
+      searchRecipies(recipeSearch);
+      nameInputEl.value = "";
+      document.getElementById("searchBtn").style.background='#05537b';
+    } else {
+        alert("Invalid entry");
+      document.getElementById("searchBtn").style.background='#05537b';
     }
-    var apiUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + ingredient.slice(0, -1);
-    console.log("this is ur apiurl: --- " + apiUrl);
+    }
+
+//function calls spooacular API to get recipe info
+var searchRecipies = function(recipe) {
+    // format the spoonacular api url
+    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${recipe}&number=12`
+      
+    // make a request to the url
     fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        if (data.drinks == "None Found") {
-          console.log("No Result")
-        } else {
-          for (var i = 0; i < data.drinks.length; i++) {
-            console.log(data.drinks[i].strDrink);
-          }
-        }
-      })});
-  }
+       // request was successful
+       if (response.ok) {
+        response.json().then(function(data) {
+          var htmlRecipeCards = recipeResults(data.results);
+          document.getElementById("cards-container").innerHTML = htmlRecipeCards;
+          var allButtons = document.querySelectorAll('.recipe-card');
+
+    for (var i = 0; i < allButtons.length; i++) {
+      allButtons[i].addEventListener('click', function() {
+      //trigger the modal to show
+      $('#food-modal').show(); 
+      });
+    }
+        });
+      } else {
+        alert('Error: Unable to connect to Spoonacular' + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert('Unable to connect to Spoonacular');
+    });
 };
 
-var addlist = function (name) {
-  var templist = document.createElement("li");
-  templist.textContent = name;
-  ingredientlist.appendChild(templist);
+ //create function to display selected recipe information
+ var selectedInfo = function(id) {
+  //format the spoonacular API url
+  var apiUrl2 = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
+  //make a request to the url
+  fetch(apiUrl2).then(function(response) {
+   if (response.ok) {
+     response.json().then(function(data) {
+       var ingredients = "";
+       for (var i = 0; i < data.extendedIngredients.length; i++ ) {
+        ingredients += (data.extendedIngredients[i].amount + " " + data.extendedIngredients[i].unit + " " + data.extendedIngredients[i].name  + ", ");
+       }
+      document.querySelector("#htmlText2").remove();
+
+      var htmlText = ` <div id="htmlText2"> 
+      <p class="card-title">${data.title}</p> 
+      <p> ${data.summary}</p> 
+      <p class="card-detail">Ingredients:</p>
+      <p> ${ingredients} </p> 
+      <p class="card-detail">Instructions:</p>
+      <p> ${data.instructions}
+      <p class="card-detail"> Source: <a class="sourceURL" href="${data.sourceUrl}">${data.sourceUrl}</a></p> 
+      <div id=btnClose> <a class="closeBtn" href="#" rel="modal:close">Close</a> </div>
+      </div>`;
+      
+      document.querySelector("#food-modal").innerHTML += htmlText;
+     });
+   }
+ })
 };
 
-var search = function (event) {
-  event.preventDefault();
-  var ingredientname = searchbar.value.trim();
-  console.log(ingredientname);
-  if (ingredientname) {
-    currentlist.push(ingredientname);
-    addlist(ingredientname);
-    displayonsearch();
-    searchbar.value = "";
-  } else {
-    alert("Please enter an ingredient username");
-  }
-};
 
-var resetsearch = function (event) {
-  event.preventDefault();
-  currentlist = [];
-  while (ingredientlist.lastElementChild) {
-    ingredientlist.removeChild(ingredientlist.lastElementChild);
-  };
-  displayonsearch();
-};
-
-searchbtn.addEventListener("click", search);
-resetbtn.addEventListener("click", resetsearch);
+//display recipies on cards formatted in the HTML
+  var recipeResults = function(results) { 
+    var htmlCards = "";
+    for (var i = 0; i < results.length; i++ ) {
+    htmlCards += `
+  <div class="cell small-3 recipe-card" onclick="selectedInfo(${results[i].id})" data-id="${results[i].id}">
+    <div class="card">
+  
+      <div class="card-section">
+      <a href="#food-modal" rel="modal:open"> <img src="${results[i].image}"> </a>
+      <a href="#food-modal" rel="modal:open"> <p class="food-title">${results[i].title}</p> </a>
+      </div>
+    </div>
+  </div>`
+    }
+    return htmlCards
+ };
+  
+userFormEl.addEventListener("submit", formSumbitHandler);
